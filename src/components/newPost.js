462b35c601c3488038f9e1d9d7b6f1bf4d332f5f
Will Cheng <willchengsters@gmail.com>
2018-04-02 22:55:32 -0700

    routing main page with thread

---
+++ src/components/newPost.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+
+export default () => {
+    return (
+        <div className="col-8 mt-2" >
+            <div className="text-center" >
+                <h1>New Post</h1>
+            </div>
+            <div>
+                <form action="">
+                    <div className="form-group" >
+                        <label for="pic">Include a file?</label>
+                        <input type="file" className="form-control-file" id="pic" />
+                        <label className="mt-4" for="post">Add a description</label>
+                        <textarea id="post" className="form-control" cols="30" rows="10" placeholder="Enter post here"></textarea>
+                        <button type="button" className="float-right mt-2 btn btn-primary" >Submit post</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+
+}
