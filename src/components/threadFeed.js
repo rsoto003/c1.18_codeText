9dac4ac68498d566710cc8ab47cefd7a69a1d2ca
Will Cheng <willchengsters@gmail.com>
2018-04-04 10:08:55 -0700

    axios calling jsbin oembed

--- src/components/threadFeed.js
+++ src/components/threadFeed.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import postData from '../data/threadItems';
 import { Link } from 'react-router-dom';
+import axios from 'axios'
 
 const blockStyle={
     display:'block'
@@ -9,7 +10,13 @@ const inlineBlockStyle={
     display:'inline-block'
 }
 
+const JSBIN_URL = 'http://jsbin.com/oembed?url=https://jsbin.com/goxeyi/6/edit?html,output';
+
 export default () => {
+
+    axios.get(JSBIN_URL).then( (res) => {
+        console.log(res)
+    })
 
     const threads = postData.map( (item, index) => {
         let postDesc = item.description.slice(0,200)
@@ -21,7 +28,9 @@ export default () => {
                 <h4>{item.title}</h4>
                 <p><small className="text-muted" > {item.author} - post ID: {item.id} </small></p>
                 <p>{postDesc}</p>
+
                 <Link style={inlineBlockStyle} to={`/thread/${item.id}`}> <p> View More </p> </Link>
+
                 <small style={blockStyle} className="text-muted">Comments</small>
                 <div className="row">
                     <div className="col">
