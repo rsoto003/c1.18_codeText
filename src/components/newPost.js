0bc984ad006e988b63cbc77c054dd01bc69b80b7
Will Cheng <willchengsters@gmail.com>
2018-04-05 16:52:16 -0700

    title input

--- src/components/newPost.js
+++ src/components/newPost.js
@@ -7,10 +7,13 @@ export default () => {
                 <h1>New Post</h1>
             </div>
             <div>
-                <form className="form-group">
-                    
+                <form className="form-group" >
+                    <div className="input-group input-group-lg">
+                        <input type="text" className="form-control" id="pic" placeholder="Add a Title" />
+                    </div>
+                        
                         <label className="mt-4" htmlFor="post">Add a description</label>
-                        <textarea id="post" className="form-control" cols="30" rows="10" placeholder="Enter post here"></textarea>
+                        <textarea id="post" className="form-control" cols="30" rows="10" placeholder="Describe your problem"></textarea>
                         <button type="button" className="float-right mt-2 btn btn-primary" >Submit post</button>
                 </form>
             </div>
