95d0eef06687189371ad07aa28608ecb5e9317b5
Ryan Soto <rsoto003@ucr.edu>
2018-04-05 12:09:18 -0700

    minor changes to index.js file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send({hellp: 'this works'})
+    console.log('i am listening');
+    res.send({
+        hello: 'this works',
+        test: 'is working',
+        dataNeeded: {
+            userID: '2342',
+            post: {
+                postID: "123",
+                comments: {
+                    commentID: "4123",
+                    comment: "this makes no sense"
+                }
+            }
+        }
+    });
 })
+
+
+app.listen(PORT);
