431a398b54eaa30613d53baa281bcb142daa1499
Will Cheng <willchengsters@gmail.com>
2018-04-09 11:08:21 -0700

    deleting unused dummy data

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default ()=> {
     return (
         <nav className="navbar navbar-dark bg-primary">
-            <Link to="/" className="navbar-brand" >context</Link>
+            <Link to="/home" className="navbar-brand" >context</Link>
             <div><input className="form-control" type="text" placeholder="search" /></div>
             <div><i className="fas fa-user-circle fa-2x"></i></div>
         </nav>
