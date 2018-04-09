9b10fc3f79c5b173045e839f86f1f666c6937181
Will Cheng <willchengsters@gmail.com>
2018-04-09 16:18:39 -0700

    adjusting css library

--- src/components/navigation/navbar.js
+++ src/components/navigation/navbar.js
@@ -6,7 +6,12 @@ export default ()=> {
         <nav className="navbar navbar-dark bg-primary">
             <Link to="/home" className="navbar-brand" >context</Link>
             <div><input className="form-control" type="text" placeholder="search" /></div>
-            <div><i className="fas fa-user-circle fa-2x"></i></div>
+            <div className="dropdown">
+                <i className="fas fa-user-circle fa-2x"></i>
+                <div className="dropdown-menu">
+                    <a href="#" className="dropdown-item"></a>
+                </div>
+            </div>
         </nav>
     )
 }
