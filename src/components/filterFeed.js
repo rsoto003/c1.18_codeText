117f9a988d985608dcfdad3f4f8e4facf693d04e
Hanran <hanrankim@gmail.com>
2018-04-13 11:09:58 -0700

    forgot to add / before home in filterfees.js

--- src/components/filterFeed.js
+++ src/components/filterFeed.js
@@ -7,19 +7,19 @@ export default ()=> {
         <div className="">                                  
             <ul className="nav justify-content-end py-3">
                 <li className="nav-item">
-                    <Link className="nav-link active" to="home/hot">Hot</Link>                    
+                    <Link className="nav-link active" to="/home/hot">Hot</Link>                    
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" to="home/popular">Popular</Link>
+                    <Link className="nav-link" to="/home/popular">Popular</Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" to="home/comments">Comments</Link>
+                    <Link className="nav-link" to="/home/comments">Comments</Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" to="home/newest">Newest</Link>
+                    <Link className="nav-link" to="/home/newest">Newest</Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" to="home/oldest">Oldest</Link>
+                    <Link className="nav-link" to="/home/oldest">Oldest</Link>
                 </li>
             </ul>
 
