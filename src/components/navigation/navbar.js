343e393fe6038c344fd3d1e4ca9919b1524ce7f0
Hanran <hanrankim@gmail.com>
2018-04-14 14:47:11 -0700

    added logo and changed nav-bg color

--- src/components/navigation/navbar.js
+++ src/components/navigation/navbar.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ProfileDropdown from './profileDropdown';
-import logo from '../assets/images/logo.svg';
+import logo from '../../assets/images/logo.svg';
+import '../../assets/css/app.css';
 
 export default ()=> {
+    const logoStyle = {
+        width: '100%'
+    }
     
     return (
-        <nav className="navbar navbar-dark bg-dark sticky-top">
-            <Link to="/home/newest" className="navbar-brand" >codecoops</Link>
-            {/* <img src={logo} alt="logo"/> */}
+        <nav className="navbar navbar-dark sticky-top new-bg">
+            <Link to="/home/newest" className="navbar-brand" ><img style={logoStyle} src={logo} alt="codingcoops logo"/></Link>           
             <div><input className="form-control" type="text" placeholder="search" /></div>
             <ProfileDropdown/>
         </nav>
