91d33a0dbeec21967e14dd0d981a5439aa1b7cf7
Hanran <hanrankim@gmail.com>
2018-04-14 14:06:33 -0700

    adding logo

--- src/components/navigation/navbar.js
+++ src/components/navigation/navbar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import ProfileDropdown from './profileDropdown'
+import ProfileDropdown from './profileDropdown';
+import logo from '../assets/images/logo.svg';
 
 export default ()=> {
     
     return (
         <nav className="navbar navbar-dark bg-dark sticky-top">
-            <Link to="/home/newest" className="navbar-brand" >context</Link>
+            <Link to="/home/newest" className="navbar-brand" >codecoops</Link>
+            {/* <img src={logo} alt="logo"/> */}
             <div><input className="form-control" type="text" placeholder="search" /></div>
             <ProfileDropdown/>
         </nav>
