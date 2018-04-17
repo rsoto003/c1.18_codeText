b11d29d74d16ecb02d2a7bf4021e21e7754cd8d4
Will Cheng <willchengsters@gmail.com>
2018-04-17 16:03:55 -0700

    going to push routes to master

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const server = express();
 const PORT = process.env.PORT || 5000;
 const keys = require('./config/keys');
 const router = express.Router();
-const PostModel = require('./models/post');
 const commentRoutes = require('./routes/commentRoutes')
 const post = require('./routes/posts')
 
@@ -12,7 +11,7 @@ server.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
@@ -27,11 +26,6 @@ mongoose.connect(keys.mongoURI, function(err, res){
     }
 })
 
-
-
-
-
-
 mongoose.connect(keys.mongoURI, function(error) {
     if (error) {
         throw error;
@@ -39,6 +33,5 @@ mongoose.connect(keys.mongoURI, function(error) {
 
     console.log("We are connected to the mlab database");
 });
-
 
 server.listen(PORT, ()=>{ console.log('server is listening to '+PORT)});
