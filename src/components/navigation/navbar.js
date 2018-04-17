dc9e7945a95f7ab6113ac9bc06444a542db3789a
Will Cheng <willchengsters@gmail.com>
2018-04-17 14:40:24 -0700

    hiding scroll bar for presentation

--- src/components/navigation/navbar.js
+++ src/components/navigation/navbar.js
@@ -11,12 +11,12 @@ export default ()=> {
             <Link to="/home/newest" className="nav" ><img className="w-100" src={logo} alt="codingcoops logo"/></Link>           
             {/* <div><input className="form-control input-lg" type="text" placeholder="search" /></div> */}
 
-            <form className="form-inline mt-2 mt-md-0 col-4"> 
+            {/* <form className="form-inline mt-2 mt-md-0 col-4"> 
                 <div className="input-group col-12">
                     <input type="text" className="form-control input-lg" placeholder="Search Post" name="search"/>                    
-                    {/* <button className="btn btn-info" type="submit"> <i className="fas fa-search"></i> </button> */}
+                    <button className="btn btn-info" type="submit"> <i className="fas fa-search"></i> </button>
                 </div>
-            </form>
+            </form> */}
             <ProfileDropdown/>
         </nav>
     )
