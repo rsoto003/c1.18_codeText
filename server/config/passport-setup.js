cd51db2b8e6f702558c32765b780cc8a2eb397d7
Will Cheng <willchengsters@gmail.com>
2018-04-17 23:25:47 -0700

    adding more stuff to schema

--- server/config/passport-setup.js
+++ server/config/passport-setup.js
@@ -1,17 +1,23 @@
 const passport = require ('passport');
-
 const GitHubStrategy = require('passport-github').Strategy;
- 
+const User = require('../models/users')
+
+
 passport.use(new GitHubStrategy({
     clientID: 'a92e4125b337edb86197',
     clientSecret: '592abb03ff862e09c389c59109769ed2850bb46a',
     callbackURL: "http://localhost:5000/auth/callback"
-    },
-    (accessToken, refreshToken, profile, cb) => {
+    },(accessToken, refreshToken, profile, done) => {
         console.log(profile)
-        User.findOrCreate({ githubId: profile.id }, (err, user) => {
-            return cb(err, user);
-        });
+        new User({
+            login: profile.login,
+            id: profile.id,
+            avatar_url: profile.avatar_id,
+            githubUrl: profile.avatar.html_url,
+            name: profile.name
+        }).save().then( (newUser)=>{
+            conosle.log('user created: ', newUser)
+        })
     }
 ));  
 
--- server/models/users.js
+++ server/models/users.js
@@ -4,10 +4,10 @@ const userSchema = new mongoose.Schema({
     login: {type: String},
     id: {type: Number},
     avatar_url: {type: String},
-    githubURL: {type: String},
-
+    githubUrl: {type: String},
     name: {type:String},
-
+    posts: {type: Array, default: []},
+    upvoted: {type:Number, default:0} 
 })
 
 module.exports = mongoose.model('userSchema', userSchema)
