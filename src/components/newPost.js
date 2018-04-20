a246a47a86b8437c7bb8448ab3af6bee564fb88c
Queen Bae <sashakyunghwa@gmail.com>
2018-04-20 10:05:27 -0700

    about page completed

--- src/components/newPost.js
+++ src/components/newPost.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
 import axios from 'axios';
-import {connect} from 'react-redux'
+
 
 class NewPost extends Component{
     constructor(props){
@@ -19,20 +19,12 @@ class NewPost extends Component{
                 display: {display:'none'},
                 isSpace: false,
                 isJsin: true
-            },
-            inputDisplays: {display: 'block'}
+            }
         }
         this.titleInputChange = this.titleInputChange.bind(this);
         this.descriptionInputChange = this.descriptionInputChange.bind(this);
         this.linkInputChange = this.linkInputChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
-    }
-    componentWillMount(){
-        if(!this.props.auth){
-            this.setState({
-                inputDisplays: {display:'none'}
-            })
-        }
     }
     titleInputChange(event){
         this.setState({
@@ -52,17 +44,21 @@ class NewPost extends Component{
 
     onSubmit(event){
         event.preventDefault()
+
         const newTitleState={...this.state.titleState};
         newTitleState.display = this.state.titleInput.length===0 ? {display:'block'} : {display:'none'};
         this.setState({
             titleState: newTitleState
         });
+
         const newDescriptionState = {...this.state.descriptionState};
         newDescriptionState.display = this.state.descriptionInput.length===0 ? {display:'block'}:{display:'none'};
         this.setState({
             descriptionState: newDescriptionState
         });
+
         const newJsbinState = this.jsbinIsValid(event);
+
         this.sumbitToDatabase(newTitleState, newDescriptionState, newJsbinState);
     }
     jsbinIsValid(event){
@@ -74,6 +70,7 @@ class NewPost extends Component{
             if (jsbinLink[i] === ' ' ){
                 isSpace = true
                 newJsbinState.display = isSpace ? {display:'block'} : {display:'none'}
+
                 event.preventDefault();
             } 
         }if (jsbinLink.length > 0){
@@ -90,8 +87,11 @@ class NewPost extends Component{
                 // http://jsbin.com/qatakap/1/edit?html,output
                 axios.get ( `https://jsbin.com/oembed?url=http://jsbin.com${subString}`).then( (res)=>{
                     console.log('res.request: ', res.request);
-                });  
+                });
+    
             }
+
+
         }
         newJsbinState.display = isJsbin ? {display:'none'} : {display:'block'} ;
         this.setState({
@@ -101,8 +101,10 @@ class NewPost extends Component{
     }
 
     sumbitToDatabase(title, description, jsbin){
+        
 
         const displays = title.display.display + description.display.display + jsbin.display;
+
         if (displays == "nonenonenone"){
             console.log('sending the payload')
             const submittedData = {
@@ -115,46 +117,37 @@ class NewPost extends Component{
                 this.props.history.push('/');
             })
         }
+
     }
 
-    isNotSignedIn(){
-        if(!this.props.auth){
-            return(
-                <div className="jumbotron">
-                    <h1 className="text-center">You need to be signed in to post a question !</h1>
-                    <a href="http://localhost:5000/auth/github" className="btn btn-dark text-white">
-                        <i className="fab fa-github fa-2x text-white pr-2"></i> Sign in with gitHub!
-                    </a>
-                </div>
-            )
-        }
-    }
 
     render(){
-
         return (
             <div className="col-md-10 col-sm-12 mt-2 offset-md-2 pl-5" >
                 <div className="text-center" >
                     <h1>New Post</h1>
                 </div>
-                {this.isNotSignedIn()}
-                <div style={this.state.inputDisplays}>
-                    <form  onSubmit={this.onSubmit} className="form-group" >
+                <div>
+                    <form onSubmit={this.onSubmit} className="form-group" >
                         <div className="input-group input-group-lg">
                             <input onChange={this.titleInputChange} type="text" className="form-control" placeholder="Add a Title" value={this.state.titleInput} />
                         </div>
+
                         <div style={this.state.titleState.display} className="alert alert-warning" role="alert"> Cannot leave title empty! </div>
+
                         <label className="mt-4" htmlFor="post">Add a description</label>
-                        <textarea style={this.state.inputDisplays} onChange={this.descriptionInputChange} id="post" className="form-control" cols="30" rows="10" placeholder="Describe your problem" value={this.state.descriptionInput}></textarea>
+                        <textarea onChange={this.descriptionInputChange} id="post" className="form-control" cols="30" rows="10" placeholder="Describe your problem" value={this.state.descriptionInput}></textarea>
+
                         <div style={this.state.descriptionState.display} className="alert alert-warning" role="alert"> Cannot leave description empty! </div>
+
                         <div className="input-group mt-5">
                             <div className="input-group-prepend">
                                 <span className="input-group-text" >JSBIN</span>
                             </div>
-                            <input  onChange={this.linkInputChange} type="text" className="form-control" id="jsbinLink" placeholder="attach a JSBIN link?" value={this.state.JSBINLink}/>
+                            <input onChange={this.linkInputChange} type="text" className="form-control" id="jsbinLink" placeholder="attach a JSBIN link?" value={this.state.JSBINLink}/>
                             <div style={this.state.jsbinState.display} className="alert alert-warning" role="alert"> This JSBIN link is not valid! </div>
                         </div>
-                        <button  className="float-right mt-2 btn btn-primary" >Submit post</button>
+                        <button className="float-right mt-2 btn btn-primary" >Submit post</button>
                     </form>
                 </div>
             </div>
@@ -163,11 +156,4 @@ class NewPost extends Component{
 
 }
 
-function mapStateToProps(state){
-    return{
-        auth: state.user.auth
-    }
-}
-
-
-export default connect(mapStateToProps)(NewPost);
+export default NewPost;
