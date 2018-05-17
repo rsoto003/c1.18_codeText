5de8d1e9f45b5a4b3eafd4f0de95e3f36026bb4c
Will Cheng <willchengsters@gmail.com>
2018-05-17 02:46:29 -0700

    sidebar init

--- src/components/navigation/navbar.js
+++ src/components/navigation/navbar.js
@@ -9,8 +9,8 @@ export default ()=> {
         
         
         <nav className="navbar navbar-dark sticky-top new-bg">
-            <Link to="/home/newest" className="nav" ><img className="w-100" src={logo} alt="codingcoops logo"/></Link>    
-
+            <Link to="/home/newest" className="nav d-none d-sm-block" ><img className="w-100" src={logo} alt="codingcoops logo"/></Link>    
+            <i className="fas fa-bars d-block d-sm-none text-white"></i>
             {/* hiding this stuff for presentation */}
             {/* <div><input className="form-control input-lg" type="text" placeholder="search" /></div> */}
             
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
+import '../assets/css/sidebar.css'
 
 export default ()=> {
     return (
-            <nav className="sidebar col-md-2 d-md-inline-block d-none nav nav-pills flex-column">
+            <nav className="sidebar col-md-2 nav nav-pills flex-column">
                     <NavLink className="nav-link font-weight-bold" to="/newPost" activeClassName="active">Post a question!</NavLink>
                     <NavLink className="nav-link font-weight-bold" to="/home/popular" activeClassName="active" >Popular</NavLink>
                     <NavLink className="nav-link font-weight-bold" to="/home/comments" activeClassName="active">Comments</NavLink>
